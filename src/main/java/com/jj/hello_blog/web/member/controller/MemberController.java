5b1336fb26cddcab96e610e0bc8b6553e03255ea
박진혁 <park@bagjinhyeog-ui-MacBookAir.local>
2024-07-06 04:56:06 +0900

    fix: MemberController 수정 @Validated 어노테이션에 잘못된 그룹을 명시했음 다시 수정

--- src/main/java/com/jj/hello_blog/web/member/controller/MemberController.java
+++ src/main/java/com/jj/hello_blog/web/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.jj.hello_blog.domain.member.entity.Member;
 import com.jj.hello_blog.domain.member.service.MemberService;
 import com.jj.hello_blog.web.member.form.MemberForm;
 import com.jj.hello_blog.web.member.form.group.save.MemberSaveGroup;
+import com.jj.hello_blog.web.member.form.group.save.MemberSaveGroupSequence;
 import com.jj.hello_blog.web.session.SessionConst;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signUp")
-    public Member signUp(@Validated(MemberSaveGroup.class) @RequestBody MemberForm memberForm, HttpSession session) {
+    public Member signUp(@Validated(MemberSaveGroupSequence.class) @RequestBody MemberForm memberForm, HttpSession session) {
         Member member = memberService.signUp(memberForm);
         session.setAttribute(SessionConst.MEMBER_KEY, member);
         return member;
